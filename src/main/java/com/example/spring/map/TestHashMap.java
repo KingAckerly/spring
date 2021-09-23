9768402f10c909e3c5ffeed30c187feb1a5ccb6e
KingAckerly <1931931123@qq.com>
2021-09-23 11:46:06 +0800

    调试hashmap转红黑树的时机

--- src/main/java/com/example/spring/map/TestHashMap.java
+++ src/main/java/com/example/spring/map/TestHashMap.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class TestHashMap {
     public static void main(String[] args) {
         //调试hashmap转红黑树的时机
-        //链表节点个数>8并且数组长度>=64,才会转红黑树
+        //链表节点个数>=8并且数组长度>=64,才会转红黑树
         //Map<A03Bean, Integer> hashmap = new HashMap<>(100);
         Map<A03Bean, Integer> hashmap = new HashMap<>();
         hashmap.put(new A03Bean(4), 0);
